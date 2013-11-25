eb795c8fbab701658ec580e36d5582e6a23ec6ef
Todd Richardson <faux.trot@gmail.com>
2013-11-25 14:45:47 -0500

    Initial Commit

---
+++ PatternLab-WPF/PatternLab-WPF.Shell/AppBootStrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using SimpleInjector;
+
+namespace PatternLab_WPF.Shell
+{
+    public class AppBootStrapper : BootstrapperBase
+    {
+        public AppBootStrapper()
+            :base(true)
+        {
+            
+        }
+
+        protected SimpleInjector.Container Container
+        {
+            get;
+            set;
+        }
+
+        protected override void Configure()
+        {
+            this.Container = new Container();
+        }
+
+        protected override void OnExit(object sender, EventArgs e)
+        {
+            Container = null;
+            base.OnExit(sender, e);
+        }
+    }
+}
---
+++ PatternLab-WPF/PatternLab-WPF.Shell/ViewModels/ShellViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace PatternLab_WPF.Shell.ViewModels
+{
+    public class ShellViewModel: Conductor<object>
+    {
+
+    }
+}
